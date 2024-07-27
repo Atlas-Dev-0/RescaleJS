3152d085aa5979c128c6937d2e0718f79400189c
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-07-27 23:02:11 +0800

    added some fixes

--- rescale-app/src/views/Rescaler.vue
+++ rescale-app/src/views/Rescaler.vue
@@ -1,36 +1,31 @@
 <template>
-  <main>
 
+  <main>
+    <!-- Uploading files -->
     <!-- get the names, details, sizes -->
     <label for="fileUpload">Upload file</label>
     <input type="file" id="fileUpload" @change="handleFileUpload" multiple />
-
-    <h3>Uploaded Images</h3>
-
+    <h3>Selected Images</h3>
     <ul>
-      <li v-for="(file, index) in uploadedFiles" :key="index">
+      <li v-for="file in uploadedFiles" :key="index">
         File Name: {{ file.name }} <br>
         Size: {{ file.size }} <br>
         Type: {{ file.type }} <br>
       </li>
     </ul>
-
   </main>
 </template>
 
-
 <script>
-import { ref } from 'vue';
 
+import { ref } from 'vue';
 export default {
 
   //TODO: 
   // Get all the files
   // Get the name of the files
   // render the files as a list
-
   setup() {
-
     const uploadedFiles = ref([]);
 
     const handleFileUpload = (event) => {
@@ -42,11 +37,7 @@ export default {
       handleFileUpload,
     }
   }
-
-
 }
-
-
 
 
 </script>
